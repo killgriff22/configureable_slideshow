83646b33295d3cf5a6de83f9f7888dd1fefacd8a
killgriff22 <kilgrif22033@gmail.com>
2024-01-02 13:22:00 -0500

    feat: Add tag functionality to image class
    This commit adds functionality to the image class for adding and removing tags. ability to skip tagged pictures and retagging pictures based on user preferences.
    
    Changes:
    - Updated .gitignore to include the "flood"
    - Updated classes.py to add various tags based on specific conditions
    - Updated tagger.py to include tagging functionality and user preferences for skipping pictures and retagging

--- classes.py
+++ classes.py
@@ -18,27 +18,31 @@ class image:
     def add_tags(self,tags:list):
         if any(tag in tags and not tag in self.tags for tag in autofavorite_artists):
             self.tags.append("favorites")
+        if any("fetish" in tag and not "fetish" == tag and (not "fetish" in self.tags or not "fetish" in tags) for tag in tags):
+            self.tags.append("fetish")
+        if any(subtag in tags and not subtag in self.tags for subtag in ["enf","exhibitionsim","exhibtion"]):
+            self.tags.append("exhibitionism") if (not "exhibitionism" in self.tags and not "exhibitionism" in tags) else None
+            self.tags.append("enf") if (not "enf" in self.tags and not "enf" in tags) else None
+            self.tags.append("exhbition") if (not "exhibition" in self.tags and not "exhibition" in tags) else None
+        if any(subtag in tags for subtag in ["natsuki","yuri_(ddlc)","sayori","monika","ddlc","doki_doki_literature_club"]):
+            self.tags.append("ddlc") if (not "ddlc" in self.tags and not "ddlc" in tags) else None
+            self.tags.append("doki_doki_literature_club") if (not "doki_doki_literature_club" in self.tags and not "doki_doki_literature_club" in tags) else None
+        if any(subtag in tags for subtag in ["callie","marie","agent_3","pearl","marina","agent_8","splatoon","shiver","frye"]):
+            self.tags.append("splatoon") if (not "splatoon" in self.tags and not "splatoon" in tags) else None
+            if any(subtag_ in tags for subtag_ in ["marina","agent_8","shiver","frye"]):
+                self.tags.append("octoling") if (not "octoling" in self.tags and not "octoling" in tags) else None
+            elif any(subtag_ in tags for subtag_ in ["callie","marie","agent_3","pearl"]):
+                self.tags.append("inkling") if (not "inkling" in self.tags and not "inkling" in tags) else None
         for tag in tags:
             if tag not in self.tags:
-                if "fetish" in tag and not "fetish" == tag and (not "fetish" in self.tags or not "fetish" in tags):
-                    self.tags.append("fetish")
-                if any(subtag in tags and not subtag in self.tags for subtag in ["enf","exhibitionsim","exhibtion"]):
-                    self.tags.append("exhibitionism") if (not "exhibitionism" in self.tags and not "exhibitionism" in tags) else None
-                    self.tags.append("enf") if (not "enf" in self.tags and not "enf" in tags) else None
-                    self.tags.append("exhbition") if (not "exhibition" in self.tags and not "exhibition" in tags) else None
-                if any(subtag in tags for subtag in ["natsuki","yuri_(ddlc)","sayori","monika","ddlc","doki_doki_literature_club"]):
-                    self.tags.append("ddlc") if (not "ddlc" in self.tags and not "ddlc" in tags) else None
-                    self.tags.append("doki_doki_literature_club") if (not "doki_doki_literature_club" in self.tags and not "doki_doki_literature_club" in tags) else None
-                if any(subtag in tags for subtag in ["callie","marie","agent_3","pearl","marina","agent_8","splatoon","shiver","frye"]):
-                    self.tags.append("splatoon") if (not "splatoon" in self.tags and not "splatoon" in tags) else None
-                    if any(subtag_ in tags for subtag_ in ["marina","agent_8","shiver","frye"]):
-                        self.tags.append("octoling") if (not "octoling" in self.tags and not "octoling" in tags) else None
-                    elif any(subtag_ in tags for subtag_ in ["callie","marie","agent_3","pearl"]):
-                        self.tags.append("inkling") if (not "inkling" in self.tags and not "inkling" in tags) else None
+                if "+" in tag and tag.replace("+","") not in self.tags:
+                    self.tags.append(tag.replace("+",""))
                 self.tags.append(tag)
         with open("metadata/" + self.name + ".tags", "w") as file:
             file.write(str(self.tags))
     def remove_tags(self,tags:List[str]):
+        if "*" in tags:
+            self.tags=[]
         for tag in tags:
             if tag in self.tags:
                 self.tags.remove(tag)
--- tagger.py
+++ tagger.py
@@ -1,13 +1,22 @@
 from classes import *
 config = load_config()
 masterPath = config['Master Folder Path']
-ans = input(f"Files in Dir: {len(os.listdir(masterPath))}\ndo you want to skip tagged pictures? (y/n): ")
-ans = True if ans.lower() == "y" else False if "n" else None
+def about_tagging():
+    skip_retag = None
+    skip_tagged = None
+    while skip_tagged==None:
+        skip_tagged = input(f"do you want to skip tagged pictures? (y/n): ")
+        skip_tagged = True if skip_tagged.lower() == "y" else False if "n" else None
+    while skip_retag==None:
+        skip_retag = input("do you want to skip retagging pictures? (y/n): ")
+        skip_retag = True if skip_retag.lower() == "y" else False if "n" else None
+    return skip_tagged, skip_retag
+skip_tagged,skip_retag = about_tagging()
 for img in os.listdir(masterPath):
     print(img)
-    if img.endswith(".jpg") or img.endswith(".png") or img.endswith(".jpeg"):
+    if img.endswith(".jpg") or img.endswith(".png") or img.endswith(".jpeg") or img.endswith(".gif") or img.endswith(".webp") or img.endswith(".mp4"):
         img_ = image(masterPath + ("/" if not masterPath[-1] == "/" else "") + img)
-        if ans and img_.tags and not "retag" in img_.tags:
+        if skip_tagged and img_.tags and (not "retag" in img_.tags or skip_retag):
             continue
         open("path", "w").write(img_.path)
         view = subprocess.Popen(["python3","./previewer.py"],stdout=subprocess.DEVNULL)
